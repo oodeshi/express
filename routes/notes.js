90cc84e42cfc091775af48dd2bb18e0d3bbec685
りょうま <ryouma@Pakus-MacBook-Air-2023.local>
2026-06-09 21:58:24 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "***********";
+const uri = "*********";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
@@ -12,7 +12,7 @@ const database = client.db('notes');
 const notes = database.collection('notes');
 
 // idが１のドキュメントを取得
-const query = { id: 1 };
+const query = { id: 2 };
 const note = await notes.findOne(query);
 
 res.json(note);
